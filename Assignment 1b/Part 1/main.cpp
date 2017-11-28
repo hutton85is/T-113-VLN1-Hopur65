2b9dba555caac3b6a4401eda091e74b3cfba6556
hutton85is <hutton85is@yahoo.com>
2017-11-28 23:13:36 +0000

    refactoring main in assignment 1B part 1

--- Assignment 1b/Part 1/main.cpp
+++ Assignment 1b/Part 1/main.cpp
@@ -6,38 +6,54 @@
 
 using namespace std;
 
+char mainMenu()
+{
+    char choose;
+    cout << "Enter what you wish to do:" << endl;
+    cout << "Press 'c' to create a hero" << endl;
+    cout << "Press 'd' to display a list of all heroes" << endl;
+    cout << "Press 'q' to quit" << endl;
+    cin >> choose;
+
+    return choose;
+}
+
+Superhero newSuperhero()
+{
+    string nafn = "";
+    int aldur = 0;
+    char ofurkraftur = 'n';
+
+    cout << "Enter name of superhero: " << endl;
+    cin >> nafn;
+    cout << "Enter age of superhero: " << endl;
+    cin >> aldur;
+    cout << "Enter special ability of superhero, flying, giant, hacker, none" << endl;
+    cin >> ofurkraftur;
+
+    Superhero s(nafn, aldur, ofurkraftur);
+
+    return s;
+}
+
 int main()
 {
     AddHero add;
     char choose;
-    do{
-        cout << "Enter what you wish to do:" << endl;
-        cout << "Press 'c' to create a hero" << endl;
-        cout << "Press 'd' to display a list of all heroes" << endl;
-        cout << "Press 'q' to quit" << endl;
-        cin >> choose;
 
+    do
+    {
+        choose = mainMenu();
         if (choose == 'c' || choose == 'C')
         {
-            string nafn = "";
-            int aldur = 0;
-            char ofurkraftur = 'n';
-
-            cout << "Enter name of superhero: " << endl;
-            cin >> nafn;
-            cout << "Enter age of superhero: " << endl;
-            cin >> aldur;
-            cout << "Enter special ability of superhero, flying, giant, hacker, none" << endl;
-            cin >> ofurkraftur;
-
-            Superhero s(nafn, aldur, ofurkraftur);
+            Superhero s = newSuperhero();
             add.write(s);
         }
         else if(choose == 'd' || choose == 'D')
         {
             cout << add << endl;
         }
-
     }while(choose != 'q');
+
     return 0;
 }
