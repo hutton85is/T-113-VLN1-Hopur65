a905759879e54e70006b94c7e8347fcd5f0d7365
Anna Margrét <annamj17@ru.is>
2017-11-30 14:50:51 +0000

    Breytingar

--- Main Assignment/Umsjon.cpp
+++ Main Assignment/Umsjon.cpp
@@ -9,46 +9,42 @@ Umsjon::~Umsjon()
     //dtor
 }
 
-void Umsjon::enterPizzaSize()
+void Umsjon::enterPizzaSize(char tmpSize[32], int tmpprice)
 {
-    char tmpSize[32];
     cout << "enter pizza size" << endl;
     cin >> tempSize;
     p.setSize(tempSize);
 }
 
-void Umsjon::enterCrust(){
-
+void Umsjon::enterCrust(char tmpSize[32], int tmpprice){
 
     cout << "What kind of crust do you want: ";
     cin >> tempCrust;
     p.setCrust(tempCrust);
 }
 
-void Umsjon::enterToppings(){
+void Umsjon::enterToppings(char tmpSize[32], int tmpprice){
 
     cout << "Enter toppings. If none press x: ";
     cin >> tempToppings;
     p.setToppings(tempToppings);
 }
 
-void Umsjon::enterMenu(){
+void Umsjon::enterMenu(char tmpSize[32], int tmpprice){
 
     cout << "Enter a new pizza: ";
     cin >> tempMenu;
     p.setPizzas(tempMenu);
 }
 
-void Umsjon::enterExtras(){
-
+void Umsjon::enterExtras(char tmpSize[32], int tmpprice){
 
     cout << "Enter a new extra: ";
     cin >> tempExtras;
     p.setExtrax(tempExtras);
 }
 
-void Umsjon::enterLocation(){
-
+void Umsjon::enterLocation(char tmpSize[32], int tmpprice){
 
     cout << "Enter new location: ";
     cin >> tempLocation;
