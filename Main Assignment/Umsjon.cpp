c21c3f09a7f5679a6b948cd0e2bc547bc4b06571
lilja16 <lilja16@ru.is>
2017-11-30 15:52:25 +0000

    breyta umsjon

--- Main Assignment/Umsjon.cpp
+++ Main Assignment/Umsjon.cpp
@@ -13,7 +13,7 @@ Umsjon::~Umsjon()
 void Umsjon::enterPizzaSize()
 {
     PizzaSize newSize;
-    cout << "enter pizza size" << endl;
+    cout << "Enter pizza size" << endl;
     cin >> newSize.p_size;
     cout << "Enter price of pizza size: " << newSize.p_size << endl;
     cin >> newSize.price;
@@ -27,35 +27,62 @@ void Umsjon::enterCrust()
     cin >> newCrust.crust_type;
     cout << "What is the price of " << newCrust.crust_type << endl;
     cin >> newCrust.price;
-    p.psize.push_back(newCrust);
+    p.menuCrust.push_back(newCrust);
 }
 
-void Umsjon::enterToppings(){
-
+void Umsjon::enterToppings()
+{
+    PizzaToppings newToppings;
     cout << "Enter toppings. If none press x: ";
-    cin >> tempToppings;
-    p.setToppings(tempToppings);
+    cin >> newToppings.toppings;
+    cout << "What is the price of " << newToppings.toppings << endl;
+    cin >> newToppings.price;
+    p.menuToppings.push_back(newToppings);
+
 }
 
-void Umsjon::enterMenu(){
-
+void Umsjon::enterMenu()
+{
+    PizzaMenu newMenu;
     cout << "Enter a new pizza: ";
-    cin >> tempMenu;
-    p.setPizzas(tempMenu);
+    cin >> newMenu.choose_pizza;
+    cout << "What is the price of " << newMenu.choose_pizza << endl;
+    cin >> newMenu.price;
+    p.menuPizzas.push_back(newMenu);
+
 }
 
-void Umsjon::enterExtras(){
-
-
+void Umsjon::enterExtras()
+{
+    PizzaExtras newExtra;
     cout << "Enter a new extra: ";
-    cin >> tempExtras;
-    p.setExtrax(tempExtras);
+    cin >> newExtra.extras_type;
+    cout << "What is the price of " << newExtra.extras_type << endl;
+    cin >> newExtra.price;
+    p.menuExtras.push_back(newExtra);
+
 }
 
-void Umsjon::enterLocation(){
-
+void Umsjon::enterLocation()
+{
+    PizzaLocations newLocation;
+    char yesOrno;
 
     cout << "Enter new location: ";
-    cin >> tempLocation;
-    p.setLocation(tempLocation);
+    cin >> newLocation.pick_up;
+    cout << "Do you want to get your order delivered? y or no? " << endl;
+    cin << yesorno << endl;
+    if ('y' == yesOrno)
+    {
+     cout << "We will be there soon with your warm pizza!" << endl;
+     newLocation.pick_up = 1;
+    }
+    else
+    {
+     cout << "You can pick up your delicious pizza at " << newLocation.pick_up << endl;
+     newLocation.pick_up = 0;
+    }
+
+    p.menuLocation.push_back(newLocation);
+
 }
