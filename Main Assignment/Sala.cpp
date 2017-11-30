4b37f1ad3bc56a3462ac1aaba8b02268514c7f41
Anna Margrét <annamj17@ru.is>
2017-11-30 15:53:31 +0000

    gleymdi ad savea

--- Main Assignment/Sala.cpp
+++ Main Assignment/Sala.cpp
@@ -2,7 +2,7 @@
 
 Sala::Sala()
 {
-    //ctor
+    lager.loadFile(lager);
 }
 
 Sala::~Sala()
@@ -10,45 +10,51 @@ Sala::~Sala()
     //dtor
 }
 
-void Sala::enterPizzaSize()
-{
+void Sala::enterPizzaSize(){
+
+    PizzaSize = newSize;
     cout << "What size do you want? " << endl;
-    p.displayVector(p.menuSize);
-    cin >> p.setSize;
+    cin >> newSize.p_size;
+    order.push_back(newSize)
 }
 
 void Sala::enterCrust(){
 
+    PizzaCrust = newCrust;
     cout << "What kind of crust would you like? ";
-    p.displayVector(p.menuCrust);
-    cin >> p.setCrust;
+    cin >> newCrust.crust_type;
+    order.push_back(newCrust);
 }
 
 void Sala::enterToppings(){
 
+    PizzaToppings =newToppings;
     cout << "Enter toppings. If none press x: ";
-    p.displayVector(p.menuToppings);
-    cin >> p.setToppings;
+    cin >> newToppings.p_toppings;
+    order.push_back(newToppings);
 }
 
 void Sala::enterMenu(){
 
+    PizzaMenu = newMenu;
     cout << "Check out our menu and choose your pizza";
-    p.displayVector(p.menuPizzas);
-    cin >> p.setMenu;
+    cin >> newMenu.choose_menu;
+    order.push_back(newMenu);
 }
 
 void Sala::enterExtras(){
 
+    PizzaExtras = newExtra;
     cout << "Would you like any extras? ";
-    p.displayVector(p.menuExtras);
-    cin >> p.setExtras;
+    cin >> newExtra.extras_type;
+    order.push_back(newExtra);
 }
 
 void Sala::enterLocation(){
 
+    PizzaLocation = newLocation;
     cout << "Where would you like to pick up your pizza? ";
-    p.displayVector(p.menuLocation);
-    cin >> p.setLocation;
+    cin >> newLocation.pick_up;
+    order.push_back(newLocation);
 }
 
--- Main Assignment/Umsjon.cpp
+++ Main Assignment/Umsjon.cpp
@@ -10,12 +10,10 @@ Umsjon::~Umsjon()
     //dtor
 }
 
-void Umsjon::enterPizzaSize(char tmpSize[32], int tmpprice)
+void Umsjon::enterPizzaSize()
 {
-<<<<<<< HEAD
-=======
     PizzaSize newSize;
->>>>>>> 91c2f4539886b891f330e0e805b450fc9c6c910a
+
     cout << "enter pizza size" << endl;
     cin >> newSize.p_size;
     cout << "Enter price of pizza size: " << newSize.p_size << endl;
@@ -23,14 +21,10 @@ void Umsjon::enterPizzaSize(char tmpSize[32], int tmpprice)
     p.psize.push_back(newSize);
 }
 
-<<<<<<< HEAD
-void Umsjon::enterCrust(char tmpSize[32], int tmpprice){
-
-=======
 void Umsjon::enterCrust()
 {
     PizzaCrust newCrust;
->>>>>>> 91c2f4539886b891f330e0e805b450fc9c6c910a
+
     cout << "What kind of crust do you want: ";
     cin >> newCrust.crust_type;
     cout << "What is the price of " << newCrust.crust_type << endl;
@@ -38,30 +32,30 @@ void Umsjon::enterCrust()
     p.psize.push_back(newCrust);
 }
 
-void Umsjon::enterToppings(char tmpSize[32], int tmpprice){
+void Umsjon::enterToppings(){
 
     cout << "Enter toppings. If none press x: ";
     cin >> tempToppings;
-    p.setToppings(tempToppings);
+
 }
 
-void Umsjon::enterMenu(char tmpSize[32], int tmpprice){
+void Umsjon::enterMenu(){
 
     cout << "Enter a new pizza: ";
     cin >> tempMenu;
-    p.setPizzas(tempMenu);
+
 }
 
-void Umsjon::enterExtras(char tmpSize[32], int tmpprice){
+void Umsjon::enterExtras(){
 
     cout << "Enter a new extra: ";
     cin >> tempExtras;
-    p.setExtrax(tempExtras);
+
 }
 
-void Umsjon::enterLocation(char tmpSize[32], int tmpprice){
+void Umsjon::enterLocation(){
 
     cout << "Enter new location: ";
     cin >> tempLocation;
-    p.setLocation(tempLocation);
+
 }
