2335d35d229bb4b73b9a6e6f46991e4e4e0bf261
Anna Margrét <annamj17@ru.is>
2017-12-01 17:35:01 +0000

    changes

--- Main Assignment/Sala.cpp
+++ Main Assignment/Sala.cpp
@@ -29,46 +29,102 @@ void Sala::enterPizzaSize(){
     {
         order.psize.push_back(lager.psize[i]);
     }
+    else{
+        cout << "Nothing available at the moment" << endl;
+    }
 }
 
 void Sala::enterCrust(){
 
     PizzaCrust newCrust;
+
     cout << "What kind of crust would you like? ";
+    for (unsigned int i = 0; i < lager.pcrust.size(); i++)
+    {
+        cout << i << " : " << lager.pcrust[i].crust_type << " " << lager.pcrust[i].price << endl;
+    }
+
     cin >> newCrust.crust_type;
     unsigned int i = 0;
     for (; i < lager.pcrust.size() || newCrust.crust_type == lager.pcrust[i].crust_type; i++)
-    order.pcrust.push_back(lager.pcrust[i]);
+
+        if(i != lager.pcrust.size())
+        {
+        order.pcrust.push_back(lager.pcrust[i]);
+        }
+        else{
+            cout << "Nothing available at the moment" << endl;
+        }
+
 }
 
 void Sala::enterToppings(){
 
     PizzaToppings newToppings;
+
     cout << "Enter toppings. If none press x: ";
+    for(unsigned int i = 0; i < lager.ptoppings.size(); i++)
+    {
+        cout << i << " : " << lager.ptoppings[i].toppings << " " << lager.ptoppings[i].price << endl;
+    }
     cin >> newToppings.toppings;
     unsigned int i = 0;
     for (; i < lager.ptoppings.size() || newToppings.toppings == lager.ptoppings[i].toppings; i++)
-    order.ptoppings.push_back(lager.ptoppings[i]);
+
+        if(i != lager.ptoppings.size())
+        {
+          order.ptoppings.push_back(lager.ptoppings[i]);
+        }
+        else{
+            cout << "Nothing available at the moment" << endl;
+        }
+
 }
 
 void Sala::enterMenu(){
 
     PizzaMenu newMenu;
+
     cout << "Check out our menu and choose your pizza";
+    for (unsigned int i = 0; i < lager.pmenu.size(); i++)
+    {
+        cout << i << " : " << lager.pmenu[i].choose_pizza << " " << lager.pmenu[i].price << endl;
+    }
     cin >> newMenu.choose_pizza;
     unsigned int i = 0;
     for (; i < lager.pmenu.size() || newMenu.choose_pizza == lager.pmenu[i].choose_pizza; i++)
-    order.pmenu.push_back(lager.pmenu[i]);
+
+        if(i != lager.pmenu.size())
+        {
+          order.pmenu.push_back(lager.pmenu[i]);
+        }
+        else{
+            cout << "Nothing available at the moment" << endl;
+        }
 }
 
 void Sala::enterExtras(){
 
     PizzaExtras newExtra;
+
     cout << "Would you like any extras? ";
+    for (unsigned int i = 0; i < lager.pextras.size(); i++)
+    {
+        cout << i << " i " << lager.pextras[i].extras_type << " " << lager.pextras[i].price << endl;
+    }
+
     cin >> newExtra.extras_type;
     unsigned int i = 0;
-    for (; i < lager.pextras.size() || newExtra.extras_type == lager.pextras[i].extras_type; i++);
-    order.pextras.push_back(lager.pextras[i]);
+    for (; i < lager.pextras.size() || newExtra.extras_type == lager.pextras[i].extras_type; i++)
+
+    if(i != lager.pextras.size())
+    {
+       order.pextras.push_back(lager.pextras[i]);
+    }
+    else{
+        cout << "Nothing available at the moment" << endl;
+    }
+
 }
 
 void Sala::enterLocation(){
