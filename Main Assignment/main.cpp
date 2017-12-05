4491bd43418e5c8306e4c3240878b29cb71fc8e9
lilja16 <lilja16@ru.is>
2017-12-05 13:44:45 +0000

    bull

--- Main Assignment/main.cpp
+++ Main Assignment/main.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+void front_page();
+
+    char who_am_i;
+    char S;
+    char E;
+    char B;
+
+void front_page ()
+{
+    cout << "To sign in as a : " << endl;
+    cout << "Supervisor choose S " << endl;
+    cout << "Employee choose E " << endl;
+    cout << "Baker choose B" << endl;
+    cin >> who_am_i << endl;
+
+
+}
+
+
 int main()
 {
     Umsjon u;
